73f60209392b2fafc4e46b6eb932d9c1d76301e5
gosho-kazuya <ketsume0211@gmail.com>
2019-03-29 17:25:09 +0900

    update

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function main() {
-  Array.from(document.querySelectorAll('div[id*=dfp-slot-]')).forEach(e => e.remove())
+function sleep() {
+  return new Promise(resolve => {
+    setTimeout(resolve, 500)
+  })
 }
 
-setTimeout(main, 100)
+async function main() {
+  const elements = Array.from(document.querySelectorAll('div[id*=dfp-slot-]'))
+  if (elements.length > 0) {
+    Array.from(document.querySelectorAll('div[id*=dfp-slot-]')).forEach(e => e.remove())
+    return
+  }
+  await sleep()
+  main()
+}
+
+main()
